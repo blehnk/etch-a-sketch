41f73695e45fdb096ad077453c37bc402f8d60d1
Ashish Nath <otakuash11apr@gmail.com>
2023-06-20 12:00:53 +0530

    Create a hover event to change color of the grid

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const container = document.getElementById('container');
 
+//Create 16 div to form 16 x 16 grid pattern
+let box;
 for(let i = 0; i < 16; i++){
-    let box = document.createElement('div');
-    box.classList.add('box', 'test');
+    box = document.createElement('div');
+    box.classList.add('box', 'hover');
     container.appendChild(box);
+}
+
+//Create an event for changing color when hovering over a div
+const hover = document.querySelectorAll('.hover');
+hover.forEach(item => item.addEventListener('mouseenter', changeColor));
+
+function changeColor(e){
+        e.target.style.backgroundColor = 'green';
 }
