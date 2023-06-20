bd5883ae8c3fa9127690aeafed965b58ecd5d5c2
Ashish Nath <otakuash11apr@gmail.com>
2023-06-20 16:10:15 +0530

    Set the size of the grid with user input

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 const boxContainer = document.getElementById('boxContainer');
 
-//Create 16 div to form 16 x 16 grid pattern
 let box;
-for(let i = 0; i < 16 * 16; i++){
-    box = document.createElement('div');
-    box.classList.add('box', 'hover');
-    boxContainer.appendChild(box);
-}
+let userInput;
+let gridSize;
 
-//Create an event for changing color when hovering over a div
-const hover = document.querySelectorAll('.hover');
-hover.forEach(item => item.addEventListener('mouseenter', changeColor));
+//Create an event to listen to a button click 
+//and give user a prompt to set the grid size when it fires
+const button = document.querySelector('.button');
+button.addEventListener('click', setGrid);
+
 
 function changeColor(e){
         e.target.style.backgroundColor = 'green';
 }
+
+
+
+//function to enter size of the grid
+function setGrid(e){
+    if (userInput !== 'undefined'){
+        for(let i = 0; i < gridSize; i++){
+            boxContainer.removeChild(boxContainer.firstChild);
+        }
+    }
+    userInput = prompt('Enter the size you want for the grid');
+    gridSize = userInput * userInput;
+    for(let i = 0; i < gridSize; i++){
+        console.log(gridSize);
+        box = document.createElement('div');
+        box.classList.add('box', 'hover');
+        let x = `${100 / userInput}%`;
+        box.style.setProperty ('--gridSize', x);
+        boxContainer.appendChild(box);
+    }
+
+    //Create an event for changing color when hovering over a div
+    const hover = document.querySelectorAll('.hover');
+    hover.forEach(item => item.addEventListener('mouseenter', changeColor));
+
+}
+
+//let gridSize = userInput * userInput;
+
+
+
